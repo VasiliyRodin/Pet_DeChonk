a463612b383bd0c2dee167bc745cd4eb03aaaa45
Vasiliy rodin <vasiliyrodin@gmail.com>
2025-05-28 16:10:30 -0700

    Added a logic module that holds the calculation logic for calories per container

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+from logic import calculate_calories_container
 
 def main():
     #gather user input
@@ -6,17 +6,10 @@ def main():
     input_calories_per_kg = input("Input the number of calories/kg from your cat food: ")
     input_grams_per_container = input("Input the amount of food in grams per container: ")
     
+
     calories_per_container = calculate_calories_container(input_calories_per_kg, input_grams_per_container)
     formated_response = "Cat food:  " + input_name + " has " + str(calories_per_container) + "calories per container" 
     print(formated_response)
     
-def calculate_calories_container(calories_per_kg, grams_per_container):
-    calories_per_container = ((int(calories_per_kg) / 1000)*int(grams_per_container))
-    return calories_per_container
-
-
-
-
-
 if __name__ == "__main__":
     main()
