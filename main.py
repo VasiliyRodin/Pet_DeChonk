91cbd7d008acfcafd2f104badcd7db572ab8a089
Vasiliy rodin <vasiliyrodin@gmail.com>
2025-05-28 16:05:36 -0700

    format

--- main.py
+++ main.py
@@ -7,16 +7,12 @@ def main():
     input_grams_per_container = input("Input the amount of food in grams per container: ")
     
     calories_per_container = calculate_calories_container(input_calories_per_kg, input_grams_per_container)
-    formated_response = "Cat food:  " + input_name + " has " + str(calories_per_container) + "calories per container" 
+    formated_response = "Cat food:  " + input_name + " has " + str(calories_per_container) + " calories per container." 
     print(formated_response)
-    
+
 def calculate_calories_container(calories_per_kg, grams_per_container):
     calories_per_container = ((int(calories_per_kg) / 1000)*int(grams_per_container))
     return calories_per_container
-
-
-
-
 
 if __name__ == "__main__":
     main()
