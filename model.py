48eb031281d882dd4a39d450ba20ad62d0ead116
Vasiliy rodin <vasiliyrodin@gmail.com>
2025-05-28 18:11:44 -0700

    created a class module for pet_food

---
+++ model.py
@@ -0,0 +1,6 @@
+class Pet_Food():
+    def __init__(self, name, cal_per_container, cal_per_kg, grams_per_container):
+        self.name = name
+        self.cal_per_container = cal_per_container
+        self.cal_per_kg = cal_per_kg
+        self.grams_per_container = grams_per_container
