b4ce163b5d85110ace2742db3af43cbbb17c0a03
Vasiliy rodin <vasiliyrodin@gmail.com>
2025-05-28 17:34:32 -0700

    Created check logic to make sure that input is over 0 and not a string

---
+++ verify_input.py
@@ -0,0 +1,13 @@
+##this is  used to check if the input of cal/kg or grams/container is a float or int. if user inputs text it returns false
+##The prompt gets passed from main to make it easier to understand in main what that function will ask for.
+
+def check_value(prompt):
+    while True:
+        try:
+            value = float(input(prompt)) ## Can't turn letters into float
+            if value <= 0:
+                print("Use a number greater than 0")
+                continue
+            return value
+        except ValueError: 
+            print("Please input a number")
